8421317c0838845b482cac180276b02e9e07f223
Haley Jacobs <haleyljacobs@gmail.com>
2018-07-14 14:03:06 -0600

    Completed Trie.js tests

--- test/Trie-test.js
+++ test/Trie-test.js
@@ -1,24 +1,24 @@
-// import { expect } from 'chai';
-// import Trie from '../lib/Trie';
-// import Node from '../lib/Node';
-// import fs from 'fs';
+import { expect } from 'chai';
+import Trie from '../lib/Trie';
+import Node from '../lib/Node';
+import fs from 'fs';
 
-// describe('trie', () => {
-//   let trie;
+describe('trie', () => {
+  let trie;
 
-//   beforeEach(() => {
-//     trie = new Trie();
-//   });
+  beforeEach(() => {
+    trie = new Trie();
+  });
 
-//   it('should exist', () => {
-//     expect(trie).to.exist;
-//   });
+  it('should exist', () => {
+    expect(trie).to.exist;
+  });
 
-//   // it('should have a root node that is an instance of Node', () => {
+  it('should have a root node that is an instance of Node', () => {
+    expect(trie.root).to.be.an.instanceof(Node);
+  });
 
-//   // });
-
-//   it('should have a count property defaulted to 0', () => {
-//     expect(trie.count).to.equal(0);
-//   });
-// });
+  it('should have a count property defaulted to 0', () => {
+    expect(trie.count).to.equal(0);
+  });
+});
