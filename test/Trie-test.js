0807415553d7d733b6d2c6a2c18d22255e83937f
Haley Jacobs <haleyljacobs@gmail.com>
2018-07-15 17:04:54 -0600

    Completed delete method

--- test/Trie-test.js
+++ test/Trie-test.js
@@ -3,6 +3,9 @@ import Trie from '../lib/Trie';
 import Node from '../lib/Node';
 import fs from 'fs';
 
+const text = "/usr/share/dict/words"
+const dictionary = fs.readFileSync(text).toString().trim().split('\n')
+
 describe('trie', () => {
   let trie;
 
@@ -23,7 +26,7 @@ describe('trie', () => {
   });
 });
 
-describe('insert', () => {
+describe('INSERT', () => {
   let trie;
 
   beforeEach(() => {
@@ -49,7 +52,7 @@ describe('insert', () => {
   })
 })
 
-describe('suggest', () => {
+describe('SUGGEST', () => {
   let trie;
 
   beforeEach(() => {
@@ -61,8 +64,71 @@ describe('suggest', () => {
   })
 })
 
-describe('select', () => {
-  it('should increase the popularity count of given word', () => {
-    expect()
+describe('POPULATE', () => {
+  let trie;
+
+  beforeEach(() => {
+    trie = new Trie();
+  })
+
+  it('should exist', () => {
+    expect(trie.populate).to.exist;
+  })
+
+  it('should add an array of words to trie', () => {
+    
+  })
+
+  it('should increase trie word count to 234371', () => {
+    trie.populate(dictionary);
+    let numberOfWords = trie.count;
+    expect(numberOfWords).to.equal(234371);
   })
 })
+
+describe('FIND', () => {
+  let trie;
+
+  beforeEach(() => {
+    trie = new Trie();
+  })
+
+  it('should exist', () => {
+    expect(trie.find).to.exist;
+  })
+
+  it('should be able to find a word in the dictionary', () => {
+    trie.insert('dog');
+    trie.find('dog');
+    expect(trie.find('dog')).to.deep.equal(trie.root.children.d.children.o.children.g)
+  })
+})
+
+describe('DELETE', () => {
+  let trie;
+
+  beforeEach(() => {
+    trie = new Trie();
+  })
+
+  it('should exist', () => {
+    expect(trie.delete).to.exist;
+  })
+
+  it('should remove completed word flag from last node', () => {
+    trie.insert('hello');
+    trie.insert('help');
+    trie.delete('help');
+
+    expect(trie.count).to.equal(1)
+  })
+
+  it('should remove any nodes that no longer lead to a completed word', () => {
+
+  })
+})
+// describe('select', () => {
+//   it('should increase the popularity count of given word', () => {
+//     expect()
+//   })
+// })
